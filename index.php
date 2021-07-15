a643f1cd78ec490101e07a95679207ecde134982
Abishek Chozhan <abishekchozhan@Abisheks-MBP.localdomain>
2021-07-15 01:32:18 -0700

    private to public

--- index.php
+++ index.php
@@ -83,13 +83,6 @@
 					<br>
 				</div>
 				<div class="col-md-2 tasks">
-					version a0.7<br><br>
-					md5 checksum:
-					46798b5cfca45c46a84b7419f8b74735<br><br>
-
-					last updated:<br> 04.12.21 at 10:19:34 AM<br><br>
-
-					last accessed:<br> 04.14.21 at 15:31:49 PM by abishekc / eshaanp
 				</div>
 			</div>
 		</div>
@@ -97,7 +90,7 @@
 		<div class="container-fluid">
 			<div class="row nav">
 				<div class="col-md-12 logo">  
-					copyright simplify healthcare. 2021.
+					simplify healthcare
 				</div>
 			</div>
 		</div>
@@ -105,31 +98,3 @@
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 	</body>
 </html>
-
-
-
-<!--<div class="row banner">
-				<div class="col-md-9 no-padding main">
-					<h1 class="tagline">MAKE HEALTHCARE<br>
-					A PRIORITY TODAY.</h1><br>
-					version a0.7<br>
-					Versioning text and onboarding material will be presented here.<br><br>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tristique ipsum<br> quis fermentum dapibus. Sed vel neque
-					vel sem tincidunt porttitor. Ut elementum enim arcu, vitae hendrerit mi auctor et. Pellentesque gravida velit et lectus placerat faucibus.<br> 
-					Phasellus maximus congue nulla, eu gravida diam fermentum a. Morbi viverra enim ante, nec pellentesque ipsum egestas eu. Suspendisse <br>
-					 lectus sapien, dictum in ipsum vitae, consequat commodo mauris. Vivamus a odio sed mi interdum posuere. Mauris vehicula sagittis urna, quis porta dui tristique non.
-				</div>
-				<div class="col-md-3 no-padding init-form">
-					<p>
-						version a0.7 <br>
-						Fill in the following information below to get tailored plans for you:
-					</p><br>
-					<form action="/plans.php" method="get">
-					  <input type="text" id="zipcode" name="zipcode" placeholder="zipcode">
-					  <input type="text" id="age" name="age" placeholder="age">
-					  <input type="text" id="income" name="income" placeholder="yearly income">
-					  <input type="text" id="income" name="budget" placeholder="budget">
-					  <input type="submit" value="SEARCH" style="font-weight: 600;">
-					</form>
-				</div>
-			</div>-->
